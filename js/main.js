d4f15598bc3950af32dd8173fe7d346df6397fcf
alihaij <a3haij@gmail.com>
2022-09-18 21:40:42 +0300

    date and complete

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-  //  const navbar = document.querySelector('.head');
-  //  window.onscroll = () => {
-  //      if (window.scrollY > 300) {
-  //          navbar.classList.add('nav-active');
-  //      } else {
-  //          navbar.classList.remove('nav-active');
-   //     }
- //   };
+//  const navbar = document.querySelector('.head');
+//  window.onscroll = () => {
+//      if (window.scrollY > 300) {
+//          navbar.classList.add('nav-active');
+//      } else {
+//          navbar.classList.remove('nav-active');
+//     }
+//   };
 
 
- //let AddBtn = document.getElementById("addBtn");
- //AddBtn.addEventListener('click', function onclick(){
- //   let list = document.createElement('ul');
+//let AddBtn = document.getElementById("addBtn");
+//AddBtn.addEventListener('click', function onclick(){
+//   let list = document.createElement('ul');
 
 
 // });
@@ -20,59 +20,83 @@ let input = document.getElementById("input");
 let list = document.getElementById("list");
 let AddBtn = document.getElementById("addBtn");
 let error = document.getElementById("error");
+let time = (new Date()).toString().split(' ').splice(1, 3).join(' ');
 // This is the array that will hold the todo list items
 
 let ListItems;
-if(localStorage.task != null){
-    ListItems = JSON.parse(localStorage.task)
+if (localStorage.task != null) {
+  ListItems = JSON.parse(localStorage.task)
 
-}else{
-    ListItems = [];
+} else {
+  ListItems = [];
 }
 
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
   let newTask = {
-    text:input.value,
+    text: input.value,
     checked: false,
-    id:Date.now(),
- }
+    id: Date.now(),
+  }
 
-if(input.value == ""){
+  if (input.value == "") {
     error.innerHTML = 'must not null';
 
- }else{
+  } else {
     ListItems.push(newTask);
-localStorage.setItem('task', JSON.stringify(ListItems))
-console.log(ListItems)
- }
-showData();
+    localStorage.setItem('task', JSON.stringify(ListItems))
+    console.log(ListItems)
+  }
+  showData();
 
 });
 
-    
-    
 
-function showData(){
+// function completTask(){
+$('body').on( 'click','.checkbox',function (e) {
+  let taskId = (e.target.id);
+  ListItems.forEach((element, index) => {
+    
+    if (element.id == taskId) {
+      if ($(`#${taskId}`).is(':checked'))  {
+        ListItems[index].checked = true;
+      }else{
+        ListItems[index].checked = false;
+      }
+      
+    }
+  })
+  localStorage.setItem('task', JSON.stringify(ListItems))
+ 
+});
+// }
 
-    let list = '';
-    for(let i = 0 ; i< ListItems.length; i++){
-        
+function showData() {
+
+  let list = '';
+  for (let i = 0; i < ListItems.length; i++) {
+
     list += `
     <li
     class="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2">
     <div class="d-flex align-items-center">
-      <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." />
-      ${ListItems[i].text}
+      <input id="${ListItems[i].id}" class=" checkbox form-check-input me-2" type="checkbox" value="" aria-label="..." />
+      <div>${ListItems[i].text}</div>
+      <div>${time}</div>
     </div>
+  
     <a href="#!" data-mdb-toggle="tooltip" title="Remove item">
+    <i class="fa-regular fa-pen-to-square"></i>
       <i class="fas fa-times text-primary"></i>
     </a>
   </li>
     `;
-    }
-    document.getElementById('list').innerHTML = list;
 
-    console.log(localStorage);
+  }
+
+  document.getElementById('list').innerHTML = list;
+  document.getElementById('input').value = "";
+
+  console.log(localStorage);
 }
--- js/main0.js
+++ js/main0.js
@@ -4,7 +4,7 @@
 mybutton = document.getElementById("back");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -19,18 +19,18 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-$(document).ready(function() {
-    $("#back").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
+$(document).ready(function () {
+  $("#back").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
 
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -44,7 +44,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 800, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
